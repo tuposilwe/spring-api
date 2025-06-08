fdc7bfeb1368acd966f5c21a4bf9459919f05917
tuposilwe <rudigermkondya@gmail.com>
2025-06-08 18:04:23 +0300

    Mapping-Objects-Using-MapStruct

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.codewithmosh.store.controllers;
 
-import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.dtos.UserDto;
+import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,19 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    public Iterable<UserDto> getAllUsers() {
+        return userRepository
+                .findAll()
+                .stream()
+                .map(userMapper::toDto)
+                .toList();
     }
-    @GetMapping("/{id}")
-    public  ResponseEntity<User> getUser(@PathVariable Long id){
-        var user =  userRepository.findById(id).orElse(null);
-       if(user == null){
-           return ResponseEntity.notFound().build();
-       }
 
-       return ResponseEntity.ok(user);
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userMapper.toDto(user));
     }
 
 }
