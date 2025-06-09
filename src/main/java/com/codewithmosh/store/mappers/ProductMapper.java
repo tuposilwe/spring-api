36c4bb0e5671a557c980c3946e05282416267acd
tuposilwe <rudigermkondya@gmail.com>
2025-06-09 23:24:01 +0300

    Building-Product-Endpoints

---
+++ src/main/java/com/codewithmosh/store/mappers/ProductMapper.java
@@ -0,0 +1,27 @@
+package com.codewithmosh.store.mappers;
+
+import com.codewithmosh.store.dtos.ProductDto;
+import com.codewithmosh.store.entities.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    @Mapping(target = "categoryId", source = "category.id")
+    ProductDto toDto(Product product);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
