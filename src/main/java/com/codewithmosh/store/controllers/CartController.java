be257d64ed6035143c90005f18d5deed4c28ba11
tuposilwe <rudigermkondya@gmail.com>
2025-06-13 21:21:20 +0300

    Documenting-APIs-with-Swagger

--- src/main/java/com/codewithmosh/store/controllers/CartController.java
+++ src/main/java/com/codewithmosh/store/controllers/CartController.java
@@ -7,6 +7,9 @@ import com.codewithmosh.store.dtos.UpdateCartItemRequest;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
 import com.codewithmosh.store.exceptions.ProductNotFoundException;
 import com.codewithmosh.store.services.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +23,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/carts")
 @AllArgsConstructor
+@Tag(name = "Carts")
 public class CartController {
     private final CartService cartService;
 
@@ -35,7 +39,11 @@ public class CartController {
     }
 
     @PostMapping("/{cartId}/items")
-    public ResponseEntity<CartItemDto> addToCart(@PathVariable UUID cartId, @RequestBody AddItemToCartRequest request) {
+    @Operation(summary = "Adds a product to the cart.")
+    public ResponseEntity<CartItemDto> addToCart(
+            @Parameter(description = "The ID of the cart.")
+            @PathVariable UUID cartId,
+            @RequestBody AddItemToCartRequest request) {
         var cartItemDto = cartService.addToCart(cartId, request.getProductId());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(cartItemDto);
