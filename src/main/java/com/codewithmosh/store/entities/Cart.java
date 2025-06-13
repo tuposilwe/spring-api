92a4aeed8845a08105e2e5e8c54a44eb1f53fde1
tuposilwe <rudigermkondya@gmail.com>
2025-06-13 19:25:06 +0300

    Refactoring-Towards-an-Object-Oriented-Design

--- src/main/java/com/codewithmosh/store/entities/Cart.java
+++ src/main/java/com/codewithmosh/store/entities/Cart.java
@@ -20,15 +20,38 @@ public class Cart {
     @Column(name = "id")
     private UUID id;
 
-    @Column(name = "date_created", insertable = false,updatable = false)
+    @Column(name = "date_created", insertable = false, updatable = false)
     private LocalDate dateCreated;
 
-    @OneToMany(mappedBy = "cart",cascade = CascadeType.MERGE,fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
     private Set<CartItem> items = new HashSet<>();
 
-    public BigDecimal getTotalPrice(){
+    public BigDecimal getTotalPrice() {
         return items.stream()
                 .map(CartItem::getTotalPrice)
-                .reduce(BigDecimal.ZERO,BigDecimal::add);
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
+
+    public CartItem getItem(Long productId) {
+        return items.stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public CartItem addItem(Product product) {
+        var cartItem = getItem(product.getId());
+        if (cartItem != null) {
+            cartItem.setQuantity(cartItem.getQuantity() + 1);
+        } else {
+            cartItem = new CartItem();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(1);
+            cartItem.setCart(this);
+            items.add(cartItem);
+        }
+        return cartItem;
+    }
+
 }
+
