26d3fa377bfa2957fe0e9784e947f3b5ad98c8fd
tuposilwe <rudigermkondya@gmail.com>
2025-06-15 19:57:05 +0300

    Checking-Out

---
+++ src/main/java/com/codewithmosh/store/dtos/CheckoutRequest.java
@@ -0,0 +1,11 @@
+package com.codewithmosh.store.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.UUID;
+@Data
+public class CheckoutRequest {
+    @NotNull(message = "Cart ID is required.")
+    private UUID cartId;
+}
