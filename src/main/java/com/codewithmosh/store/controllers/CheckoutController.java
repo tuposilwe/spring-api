33b999a5b542ca75342352d1b0937e81d4c07917
tuposilwe <rudigermkondya@gmail.com>
2025-06-15 22:06:37 +0300

    Improving-Error-Handling

--- src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
+++ src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
@@ -2,6 +2,7 @@ package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.CheckoutRequest;
 import com.codewithmosh.store.dtos.CheckoutResponse;
+import com.codewithmosh.store.dtos.ErrorDto;
 import com.codewithmosh.store.entities.Order;
 import com.codewithmosh.store.entities.OrderItem;
 import com.codewithmosh.store.entities.OrderStatus;
@@ -36,13 +37,13 @@ public class CheckoutController {
         var cart = cartRepository.getCartWithItems(request.getCartId()).orElse(null);
         if (cart == null) {
             return ResponseEntity.badRequest().body(
-                    Map.of("error", "Cart not found")
+                    new ErrorDto("Cart not found")
             );
         }
 
         if (cart.getItems().isEmpty()) {
             return ResponseEntity.badRequest().body(
-                    Map.of("error", "Cart is empty.")
+                    new ErrorDto("Cart is empty.")
             );
         }
 
