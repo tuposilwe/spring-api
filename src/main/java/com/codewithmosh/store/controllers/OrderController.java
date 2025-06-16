ebe54deaf22542cec9539eb77fcd4fdf136c77b4
tuposilwe <rudigermkondya@gmail.com>
2025-06-16 11:25:08 +0300

    Getting-a-Single-Order

--- src/main/java/com/codewithmosh/store/controllers/OrderController.java
+++ src/main/java/com/codewithmosh/store/controllers/OrderController.java
@@ -1,11 +1,14 @@
 package com.codewithmosh.store.controllers;
 
+import com.codewithmosh.store.dtos.ErrorDto;
 import com.codewithmosh.store.dtos.OrderDto;
+import com.codewithmosh.store.exceptions.OrderNotFoundException;
 import com.codewithmosh.store.services.OrderService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,4 +22,24 @@ public class OrderController {
     public List<OrderDto> getAllOrders() {
         return orderService.getAllOrders();
     }
+
+    @GetMapping("/{orderId}")
+    public OrderDto getOrder(@PathVariable("orderId") Long orderId) {
+        return orderService.getOrder(orderId);
+    }
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleOrderNotFound(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorDto(ex.getMessage()));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorDto> handleAccessDenied(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(new ErrorDto(ex.getMessage()));
+    }
+
 }
