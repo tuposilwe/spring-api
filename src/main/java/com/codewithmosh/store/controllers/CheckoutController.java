0c232353f4ee7d7638e8c0758daa3227ccc9e18b
tuposilwe <rudigermkondya@gmail.com>
2025-06-16 21:45:00 +0300

    Updating-Order-Status

--- src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
+++ src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
@@ -3,11 +3,14 @@ package com.codewithmosh.store.controllers;
 import com.codewithmosh.store.dtos.CheckoutRequest;
 import com.codewithmosh.store.dtos.CheckoutResponse;
 import com.codewithmosh.store.dtos.ErrorDto;
+import com.codewithmosh.store.entities.OrderStatus;
 import com.codewithmosh.store.exceptions.CartEmptyException;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
 import com.codewithmosh.store.exceptions.PaymentException;
+import com.codewithmosh.store.repositories.OrderRepository;
 import com.codewithmosh.store.services.CheckoutService;
 import com.stripe.exception.SignatureVerificationException;
+import com.stripe.model.PaymentIntent;
 import com.stripe.net.Webhook;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +27,7 @@ public class CheckoutController {
 
     @Value("${stripe.webhookSecretKey}")
     private String webhookSecretKey;
+    private final OrderRepository orderRepository;
 
     @PostMapping
     public CheckoutResponse checkout(
@@ -45,6 +49,15 @@ public class CheckoutController {
             switch (event.getType()) {
                 case "payment_intent.succeeded" -> {
                     // Update order status (PAID)
+                    var paymentIntent = (PaymentIntent) stripeObject;
+                    if (paymentIntent != null) {
+                        var orderId = paymentIntent.getMetadata().get("order_id");
+                        var order = orderRepository.findById(Long.valueOf(orderId)).orElseThrow();
+                        order.setStatus(OrderStatus.PAID);
+                        orderRepository.save(order);
+                    }
+
+
                 }
                 case "payment_intent.failed" -> {
                     // Update order status (FAILED)
