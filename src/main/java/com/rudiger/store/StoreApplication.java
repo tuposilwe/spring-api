9f2fa400ba1ea28d4e45e249dcd72d29728e8810
tuposilwe <rudigermkondya@gmail.com>
2025-06-17 10:38:56 +0300

    Managing-Environments-with-Spring-Profiles

--- src/main/java/com/rudiger/store/StoreApplication.java
+++ src/main/java/com/rudiger/store/StoreApplication.java
@@ -2,10 +2,8 @@ package com.rudiger.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class StoreApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class, args);
     }
